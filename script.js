131d1d7fd8ff4704db85126d0ce61d083669cc7b
Pavel-Shebashev <90134321+Pavel-Shebashev@users.noreply.github.com>
2022-06-17 20:45:18 +0300

    fix: relajout header for firefox

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function bake(degrees) {
-  let message;
-  if (degrees > 500) {
-  message = "I'm not a nuclear reactor!";
-  } else if (degrees < 100) {
-  message = "I'm not a refrigerator!";
-  } else {
-  message = "That's a very comfortable temperature for me.";
-  setMode("bake");
-setTemp(degrees);
-}
-return message;
-}
-let status = bake(350);
+
